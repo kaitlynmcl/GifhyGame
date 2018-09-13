e84c746999156bbc63586ca595bd68fbf9596952
Kaitlyn McLaughlin <kaitlyn@Kaitlyns-MacBook-Air.local>
2018-09-13 10:43:53 -0400

    images still/animate

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # GifhyGame
-A gifhy generator that generates images of my favorite musicians. 
+A gifhy generator that generates images for members of the Sad Boys Club&trade;. 
+Do you want to join the club? Add your own Sad Boy&trade;. 
--- game.js
+++ game.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
 
+                                                //ARRAY TO STORE GIFPHY IMAGES 
 var topics = ["GERARD WAY", "ROBERT SMITH",  "SKRILLEX", "BRIGHT EYES", "SUFJAN STEVENS", "MORRISEY", "LEONARD COHEN"]; 
 for (var i = 0; i <topics.length; i++) {
     var artistAdd = $("<button>");
@@ -9,9 +10,11 @@ for (var i = 0; i <topics.length; i++) {
     $("#topics").append(artistAdd);
 }   
 
+                                                //ON CLICK TO PULL IMAGES FROM GIPHY EACH TIME USER CLICKS
 $("button").on("click", function() {
     var artist = $(this).attr("data-name");
     var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + artist + "&api_key=XDPcOeI4M9yKEpttK33FxsuypoDjJGKt&limit=10"; 
+   
 
     $.ajax({
       url: queryURL,
@@ -25,16 +28,31 @@ $("button").on("click", function() {
           var rating = results[i].rating;
           var p = $("<p>").text("Rating: " + rating);
 
-          var artistImage = $("<img>");
-          artistImage.attr("src", results[i].images.fixed_height.url);
+          var artistImage = $("<img>");        //ADDING ATTRIBUTES TO ALLOW FOR STILL/MOVING IMAGES
+          artistImage.attr("src", results[i].images.fixed_height_still.url);
+          artistImage.attr("data-state", "still");
+          artistImage.attr("data-still", results[i].images.fixed_height_still.url);
+          artistImage.attr("data-animate", results[i].images.fixed_height.url);
+          artistImage.addClass("artistImage");
           gifDiv.prepend(p);
           gifDiv.prepend(artistImage);
 
           $("#gifs-station").prepend(gifDiv);
         }
-  
-     })
+        $(".artistImage").on("click", function() {
+            var state = $(this).attr("data-state");
+            console.log(state);
 
+            if (state === "still") {
+              $(this).attr("src", $(this).attr("data-animate"));
+              $(this).attr("data-state", "animate");
+            } else {
+              $(this).attr("src", $(this).attr("data-still"));
+              $(this).attr("data-state", "still");
+            }
+          });
+    
+     })
+    
     });
-   
-  });
+});
