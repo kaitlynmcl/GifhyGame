0b15e4b22a3b9d9a882c7ab0d1a040e54a1dbec8
Kaitlyn McLaughlin <kaitlyn@Kaitlyns-MacBook-Air.local>
2018-09-13 14:35:33 -0400

    reconfigure for form

--- game.js
+++ game.js
@@ -1,61 +1,83 @@
 $(document).ready(function() {
 
-                                                //ARRAY TO STORE GIFPHY IMAGES 
+                                                        //ARRAY TO STORE GIFPHY IMAGES 
 var topics = ["GERARD WAY", "ROBERT SMITH",  "SKRILLEX", "BRIGHT EYES", "SUFJAN STEVENS", "MORRISEY", "LEONARD COHEN"]; 
+                           
+                                                        //FUNCTION THAT RE-RENDERS HTML TO DISPLAY 
+function displayGifphys() {
+    var artist = $(this).attr("data-name");
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + artist + "&api_key=XDPcOeI4M9yKEpttK33FxsuypoDjJGKt&limit=10"; 
 
+$.ajax({                                                //CREATING AJAX CALL FOR SPECIIFC SADBOY BUTTON
+    url: queryURL,
+    method: "GET"
+    }).done(function(response) {
+    $("#gifs-station").empty();                         //CLEARS OUT IMAGES
 
+    var results = response.data;                        //RETRIEVING GIPHY DATA
+    console.log(response);
 
-for (var i = 0; i <topics.length; i++) {
+    for(var i = 0; i < results.length; i++) {           //LOOPS THROUGH RESULTS
+
+    var gifDiv = $("<div>");                            //CREATING A DIV TO HOLD THE GIPHYS 
+    gifDiv.addClass("sadboyPics");                      //ADDS CLASS FOR STYLE (CSS)
+    var rating = results[i].rating;                     //STORING THE RATING DATA
+    var p = $("<p>").text("Rating: " + rating);         //DISPLAYS RATING DATA 
+
+    var artistImage = $("<img>");                       //ADDING ATTRIBUTES TO ALLOW FOR STILL/MOVING IMAGES
+    artistImage.attr("src", results[i].images.fixed_height_still.url);
+    artistImage.attr("data-state", "still");
+    artistImage.attr("data-still", results[i].images.fixed_height_still.url);
+    artistImage.attr("data-animate", results[i].images.fixed_height.url);
+    artistImage.addClass("artistImage");
+    gifDiv.prepend(p);                                  //DISPLAYS RATING
+    gifDiv.prepend(artistImage);                        //DISPLAYS SADBOY IMAGES
+    $("#gifs-station").prepend(gifDiv);
+}
+    $(".artistImage").on("click", function() {
+        var state = $(this).attr("data-state");
+        console.log(state);
+    
+    if (state === "still") {                            //ALLOWS FOR STILLNESS/CHANGES
+        $(this).attr("src", $(this).attr("data-animate"));
+        $(this).attr("data-state", "animate");
+        } else {
+        $(this).attr("src", $(this).attr("data-still"));
+        $(this).attr("data-state", "still");
+        }
+    });
+});
+
+}
+
+function renderButtons() {                              //FUNCTION TO DISPLAY BUTTONS 
+
+    $("#sadBoyButtons").empty();
+
+    for (var i = 0; i <topics.length; i++) {
     var artistAdd = $("<button>");
     artistAdd.addClass("artist");
     artistAdd.attr("data-name", topics[i]);
     artistAdd.text(topics[i]);
-    $("#topics").append(artistAdd);
-}   
+    $("#sadBoyButtons").append(artistAdd);
+    }   
+}
+$("#addSadBoy").on("click", function(event){            //ALLOWS TO UPDATE FORM WITH NEW SADBOY
+    event.preventDefault();
 
-                                                //ON CLICK TO PULL IMAGES FROM GIPHY EACH TIME USER CLICKS
-$("button").on("click", function() {
-    var artist = $(this).attr("data-name");
-    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + artist + "&api_key=XDPcOeI4M9yKEpttK33FxsuypoDjJGKt&limit=10"; 
-    $("#gifs-station").empty();                //CLEARS OUT GIFS-STATION DIV TO MAKE ROOM FOR NEXT GROUP OF IMAGES 
+    // This line of code will grab the input from the textbox
+    var sadBoySelect = $("#musician-input").val().trim();
 
-    $.ajax({
-      url: queryURL,
-      method: "GET"
-    }).then(function(response) {
-        var results = response.data;
+    // The anime from the textbox is then added to our array
+    topics.push(sadBoySelect);
 
-        for (var i = 0; i < results.length; i++) {
-          var gifDiv = $("<div class='item'>");
+    // Calling renderButtons which handles the processing of our anime array
+    renderButtons();
+  });
 
-          var rating = results[i].rating;
-          var p = $("<p>").text("Rating: " + rating);
+  // Adding click event listeners to all elements with a class of "anime"
+  $(document).on("click", ".artist", displayGifphys);   //SELECTS ARTIST CLASS TO GENERATE BUTTONS
 
-          var artistImage = $("<img>");        //ADDING ATTRIBUTES TO ALLOW FOR STILL/MOVING IMAGES
-          artistImage.attr("src", results[i].images.fixed_height_still.url);
-          artistImage.attr("data-state", "still");
-          artistImage.attr("data-still", results[i].images.fixed_height_still.url);
-          artistImage.attr("data-animate", results[i].images.fixed_height.url);
-          artistImage.addClass("artistImage");
-          gifDiv.prepend(p);
-          gifDiv.prepend(artistImage);
-
-          $("#gifs-station").prepend(gifDiv);
-        }
-        $(".artistImage").on("click", function() {
-            var state = $(this).attr("data-state");
-            console.log(state);
-
-            if (state === "still") {
-              $(this).attr("src", $(this).attr("data-animate"));
-              $(this).attr("data-state", "animate");
-            } else {
-              $(this).attr("src", $(this).attr("data-still"));
-              $(this).attr("data-state", "still");
-            }
-          });
-    
-     })
-    
-    });
+  // Calling the renderButtons function to display the intial buttons
+  renderButtons();
 });
